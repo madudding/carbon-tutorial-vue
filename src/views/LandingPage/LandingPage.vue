ef4650d5aaf99f91047d76ffa187a16662fb6c66
Mark Dudding <mark@appfusions.com>
2020-05-18 23:09:29 -0500

    feat(tutorial): complete step 2

--- src/views/LandingPage/LandingPage.vue
+++ src/views/LandingPage/LandingPage.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>LANDING PAGE</div>
+  <div class="bx--grid bx--grid--full-width landing-page">
+    <div class="bx--row landing-page__banner">
+      <div class="bx--col-lg-16">1</div>
+      <cv-breadcrumb noTrailingSlash>
+              <cv-breadcrumb-item>
+        <cv-link href="/">Getting started</cv-link>
+      </cv-breadcrumb-item>
+    </cv-breadcrumb>
+    <h1 class="landing-page__heading">Design &amp; build with Carbon</h1>
+  </div>
+    </div>
+    <div class="bx--row landing-page__r2">
+      <div class="bx--col-md-4 bx--col-lg-7">7/16</div>
+      <div class="bx--col-md-4 bx--offset-lg-1 bx--col-lg-8">8/16</div>
+    </div>
+    <div class="bx--row landing-page__r3">
+      <div class="bx--col-md-4 bx--col-lg-4">1/4</div>
+      <div class="bx--col-md-4 bx--col-lg-4">1/4</div>
+      <div class="bx--col-md-4 bx--col-lg-4">1/4</div>
+      <div class="bx--col-md-4 bx--col-lg-4">1/4</div>
+    </div>
+  </div>
 </template>
